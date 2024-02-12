dc3dd3d0973ef8de9e882b23f61031430e19849f
Jesse Lee <mienaikoe@gmail.com>
2024-02-11 23:51:55 -0500

    csv-ify

--- src/fortunes.ts
+++ src/fortunes.ts
@@ -1,24 +1,35 @@
-enum FortuneType {
-    words,
-    video
-}
+import Papa from "papaparse";
+
+const FortuneType = {
+    words: "words",
+    video: "video",
+};
 
 type WordsOfWisdom = {
-    type: FortuneType.words,
+    type: typeof FortuneType.words,
     message: string
 }
 
 type VideoMessage = {
-    type: FortuneType.video,
-    url: string
+    type: typeof FortuneType.video,
+    message: string
 }
 
 export type Fortune = WordsOfWisdom | VideoMessage;
 
-export const fortunes: Fortune[] = [
-    { type: FortuneType.words, message: "The early bird gets the worm" },
-    { type: FortuneType.video, url: "https://vimeo.com/818539277" }
-];
+// export const fortunes: Fortune[] = [
+//     { type: FortuneType.words, message: "The early bird gets the worm" },
+//     { type: FortuneType.video, url: "https://vimeo.com/818539277" }
+// ];
+
+
+export const getFortunes: () => Promise<Fortune[]> = async () => {
+    const response = await fetch("/public/data.csv");
+    const responseText = await response.text();
+    const responseCsv = Papa.parse(responseText, {header: true, skipEmptyLines: true});
+    return responseCsv.data as Fortune[];
+}
+
 
 export const isVideo = (fortune: Fortune): fortune is VideoMessage => {
     return fortune.type === FortuneType.video;
